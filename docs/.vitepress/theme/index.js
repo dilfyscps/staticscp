38fe117a581f381274ba88442fa2dc7544e31616
dilfyscps <jayce.curran3@icloud.com>
2025-11-10 05:52:20 -0800

    analytics

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,8 +1,17 @@
-    // .vitepress/theme/index.js
-    import DefaultTheme from 'vitepress/theme'
-    import './custom.css' // Import your custom CSS
+// .vitepress/theme/index.js
+import DefaultTheme from 'vitepress/theme'
+import './custom.css'
+import { injectAnalytics } from '@vercel/analytics'
 
-    export default {
-      ...DefaultTheme,
-      // You can further enhance the app or override components here if needed
+export default {
+  ...DefaultTheme,
+  setup() {
+    // Call DefaultTheme's setup if it exists
+    if (DefaultTheme.setup) DefaultTheme.setup()
+
+    // Inject analytics only on the client
+    if (typeof window !== 'undefined') {
+      injectAnalytics()
     }
+  },
+}
