aef7df5a00890902853885aca52aa9c6342ca499
dilfyscps <jayce.curran3@icloud.com>
2025-11-11 08:55:07 -0800

    yes

--- docs/.vitepress/theme/Layout.vue
+++ docs/.vitepress/theme/Layout.vue
@@ -1,14 +1,39 @@
 <template>
   <div class="home-centered">
-    <Content/>
+    <!-- Dismissible Warning Banner -->
+    <div
+      v-if="showWarning"
+      class="search-warning"
+      style="background: #ffcc00; color: #000; text-align: center; padding: 0.5em; font-weight: bold; position: relative;"
+    >
+      ⚠️ Search is temporarily down. We’re working to fix it!
+      <button
+        @click="dismiss"
+        style="position: absolute; right: 10px; top: 5px; background: none; border: none; font-weight: bold; cursor: pointer;"
+      >
+        ✕
+      </button>
+    </div>
+
+    <!-- Existing content -->
+    <Content />
   </div>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import Content from '@theme/Layout.vue'
 
 export default defineComponent({
-  components: { Content }
+  components: { Content },
+  setup() {
+    const showWarning = ref(true)
+
+    const dismiss = () => {
+      showWarning.value = false
+    }
+
+    return { showWarning, dismiss }
+  }
 })
 </script>
